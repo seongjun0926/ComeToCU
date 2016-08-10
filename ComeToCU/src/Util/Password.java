89c795736841e686fb0a54a65b5c85cff7482933
seongjun0926 <seongjun0926@naver.com>
2016-08-11 00:25:39 +0900

    ㅇ

--- ComeToCU/src/Util/Password.java
+++ ComeToCU/src/Util/Password.java
@@ -3,19 +3,6 @@ package Util;
 import java.security.*;
 
 public class Password {
-	public String createHash(String data) throws Exception{
-		if(data==null){
-			throw new NullPointerException();
-		}
-		
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		byte[] raw = md.digest(data.getBytes("UTF-8"));
-		
-		StringBuffer result = new StringBuffer();
-		for(int i=0;i<raw.length;i++){
-			result.append(Integer.toHexString(raw[i]&0xff));
-		}
-		return result.toString();
-	}
+
 	
 }
